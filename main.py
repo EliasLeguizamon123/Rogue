b18fa1ef6194e902ff32c55ded496d89d3967d6a
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2022-07-14 09:41:22 -0300

    add movement

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
 import tcod
 
-def main():
-  #Create Context and show it
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
+def main() -> None:
+
   width = 80
   height = 50
-  
+
+  playerX = int(width / 2)
+  playerY = int(height / 2)
+
+  #Define the font and Size  
   tileset = tcod.tileset.load_tilesheet(
     'dejavu10x10_gs_tc.png', 32, 8, tcod.tileset.CHARMAP_TCOD
   )
 
+  event_handler = EventHandler()
+
+  #Create my window and define h, w, vsync, etc. 
   with tcod.context.new_terminal(
     width,
     height,
@@ -18,13 +28,26 @@ def main():
     vsync=True,
   ) as context:
     root = tcod.Console(width, height, order="F")
+    #Loop game
     while True:
-      root.print(x=1, y=1, string="@")
-
+      #Where the @ start
+      root.print(x=playerX, y=playerY, string="@")
+      #Print the screen and update
       context.present(root)
 
+      root.clear()
+
+      #Capture User input
       for event in tcod.event.wait():
-        if event.type == "QUIT":
+        action = event_handler.dispatch(event)
+        if action is None:
+          continue
+        #Movement
+        if isinstance(action, MovementAction):
+          playerX += action.dx
+          playerY += action.dy
+        #Quit
+        elif isinstance(action, EscapeAction):
           raise SystemExit()
 
 if __name__ == "__main__":
